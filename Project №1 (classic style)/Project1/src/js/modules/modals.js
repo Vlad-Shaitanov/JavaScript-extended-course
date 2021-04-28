48809b055e54e3dc47775891438988f26533d4b9
Vladislav <kelsenelenelvial@mail.ru>
2021-04-28 16:10:10 +0300

    added animation & fixing scroll. production build

--- Project №1 (classic style)/Project1/src/js/modules/modals.js
+++ Project №1 (classic style)/Project1/src/js/modules/modals.js
@@ -14,7 +14,8 @@ const modals = () => {
 		const trigger = document.querySelectorAll(triggerSelector),
 			modal = document.querySelector(modalSelector),
 			close = document.querySelector(closeSelector),
-			windows = document.querySelectorAll("[data-modal]");//Все модальные окна
+			windows = document.querySelectorAll("[data-modal]"),//Все модальные окна
+			scroll = calcScroll();//Вычисленная ширина скролла
 
 		//Открытие модалки
 		trigger.forEach(item => {
@@ -32,6 +33,9 @@ const modals = () => {
 				modal.style.display = "block";
 				//Запрещаем скролл страницы при открытом модальном окне
 				document.body.style.overflow = "hidden";
+				/*При открытии модалки будет добавлен margin-right, чтобы
+				убрать перемещение страницы в бок при отключении скролла*/
+				document.body.style.marginRight = `${scroll}px`;
 				//document.body.classList.add("modal-open");//Класс бутстрапа
 			});
 
@@ -46,6 +50,7 @@ const modals = () => {
 			modal.style.display = "none";
 			//Возвращаем скролл в дефолтное значение
 			document.body.style.overflow = "";
+			document.body.style.marginRight = "0px";//Убираем правый марджин
 			//document.body.classList.remove("modal-open");//Класс бутстрапа
 		});
 
@@ -61,6 +66,8 @@ const modals = () => {
 				modal.style.display = "none";
 				//Возвращаем скролл в дефолтное значение
 				document.body.style.overflow = "";
+				document.body.style.marginRight = "0px";//Убираем правый марджин
+
 				//document.body.classList.remove("modal-open");//Класс бутстрапа
 			}
 		});
@@ -71,6 +78,26 @@ const modals = () => {
 			document.querySelector(selector).style.display = "block";
 			document.body.style.overflow = "hidden";
 		}, time);
+	}
+
+	//Подсчет ширины скролла сбоку страницы
+	function calcScroll() {
+		let div = document.createElement("div");
+
+		div.style.width = "50px";
+		div.style.height = "50px";
+		div.style.overflowY = "scroll";
+		div.style.visibility = "hidden";
+
+		document.body.appendChild(div);
+
+		/*Ширина скролла равна полной ширине минус ширина клиента*/
+		let scrollWidth = div.offsetWidth - div.clientWidth;
+
+		//Удаляем созданный элемент
+		div.remove();
+
+		return scrollWidth;
 	}
 
 	bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close");
