f919eb8468ee6e0be5eb6edf3169b5e10f05ba45
Vladislav <kelsenelenelvial@mail.ru>
2021-05-01 18:36:21 +0300

    gift modal window

--- Project №2 (classic style)/src/js/modules/modals.js
+++ Project №2 (classic style)/src/js/modules/modals.js
@@ -3,13 +3,17 @@
 
 
 const modals = () => {
+	//Состояние (Была ли нажата хоть какая-то кнопка на странице)
+	let btnPressed = false;
+
 	//функция привязки модального окна к определенному селектору
-	function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+	function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false) {
 		/*
 		1арг - элемент, нак отором срабатывает функция
 		2арг - нужное можальное окно
 		3арг - элемент для закрытия модального окна
-		4арг - параметр, определяющий, может ли быть закрыта модалка при клике на оверлей*/
+		4арг - параметр, определяющий, нужно ли убрать триггер со страницы при
+			   закрытии модального окна*/
 
 		const trigger = document.querySelectorAll(triggerSelector),
 			modal = document.querySelector(modalSelector),
@@ -24,10 +28,19 @@ const modals = () => {
 					event.preventDefault();
 				}
 
+				btnPressed = true;//Юзер кликнул на одну из кнопок
+
 				windows.forEach(item => {
 					//Закрытие сразу всех модальных окон
 					item.style.display = "none";
+					item.classList.add("animated", "fadeIn");//Анимация появления модалок
 				});
+
+				if (destroy) {
+					/*При наличии метки, триггер будет удален со страницы при
+					закрытии модального окна*/
+					item.remove();
+				}
 
 				//Показываем модальное окно
 				modal.style.display = "block";
@@ -56,7 +69,7 @@ const modals = () => {
 
 		modal.addEventListener("click", (event) => {
 			//При нажании на оверлей модальное окно закроется
-			if (event.target === modal && closeClickOverlay) {
+			if (event.target === modal) {
 				windows.forEach(item => {
 					//Закрытие сразу всех модальных окон
 					item.style.display = "none";
@@ -93,6 +106,9 @@ const modals = () => {
 			if (!display) {
 				document.querySelector(selector).style.display = "block";
 				document.body.style.overflow = "hidden";
+
+				let scroll = calcScroll();//Вычисленная ширина скролла
+				document.body.style.marginRight = `${scroll}px`;
 			}
 
 
@@ -119,8 +135,23 @@ const modals = () => {
 		return scrollWidth;
 	}
 
+	//Выполнить при скролле до конца страницы
+	function openByScroll(selector) {
+		//Проверяем долистал ли юзер до конца страницы
+		window.addEventListener("scroll", () => {
+			if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight)) {
+				/*Если ни одна кнопка на странице не нажата и (Сколько px
+					пользователь отлистал сверху + тот контент, который
+					сейчас виден юзеру >= полной высоты страницы)*/
+				document.querySelector(selector).click();//Программный вызов события
+			}
+		});
+	}
+
 	bindModal(".button-design", ".popup-design", ".popup-design .popup-close");
 	bindModal(".button-consultation", ".popup-consultation", ".popup-consultation .popup-close");
+	bindModal(".fixed-gift", ".popup-gift", ".popup-gift .popup-close", true);
+	openByScroll(".fixed-gift");
 
 	showModalByTime(".popup-consultation", 6000);
 };
