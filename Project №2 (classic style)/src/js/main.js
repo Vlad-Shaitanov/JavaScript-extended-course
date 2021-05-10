4af746530a68d94a4505c4fed809a00b3a38819c
Vladislav <kelsenelenelvial@mail.ru>
2021-05-10 15:34:04 +0300

    added filter

--- Project №2 (classic style)/src/js/main.js
+++ Project №2 (classic style)/src/js/main.js
@@ -7,6 +7,7 @@ import mask from "./modules/mask";
 import checkTextInputs from "./modules/checkTextInputs";
 import showMoreStyles from "./modules/showMoreStyles";
 import calc from "./modules/calc";
+import filter from "./modules/filter";
 
 window.addEventListener("DOMContentLoaded", () => {
 
@@ -26,4 +27,6 @@ window.addEventListener("DOMContentLoaded", () => {
 	showMoreStyles(".button-styles", "#styles .row");
 
 	calc("#size", "#material", "#options", ".promocode", ".calc-price");
+
+	filter();
 });
---
+++ Project №2 (classic style)/src/js/modules/filter.js
@@ -0,0 +1,84 @@
+"use strict";
+
+const filter = () => {
+	const menu = document.querySelector('.portfolio-menu'),
+		items = menu.querySelectorAll('li'),
+		btnAll = menu.querySelector('.all'),
+		btnLovers = menu.querySelector('.lovers'),
+		btnChef = menu.querySelector('.chef'),
+		btnGirl = menu.querySelector('.girl'),
+		btnGuy = menu.querySelector('.guy'),
+		btnGrandmother = menu.querySelector('.grandmother'),
+		btnGranddad = menu.querySelector('.granddad'),
+		wrapper = document.querySelector('.portfolio-wrapper'),
+		markAll = wrapper.querySelectorAll('.all'),
+		markGirl = wrapper.querySelectorAll('.girl'),
+		markLovers = wrapper.querySelectorAll('.lovers'),
+		markChef = wrapper.querySelectorAll('.chef'),
+		markGuy = wrapper.querySelectorAll('.guy'),
+		no = document.querySelector('.portfolio-no');
+
+	//Фильтруем контент по типу
+	const typeFilter = (markType) => {
+		//Сначала скрываем все
+		markAll.forEach(mark => {
+			mark.style.display = "none";
+			mark.classList.remove("animated", "fadeIn");
+		});
+
+		no.style.display = "none";//Так же скрываем информационный блок
+		no.classList.remove("animated", "fadeIn");
+
+		//Показываем нужный тип контента
+		if (markType) {
+			markType.forEach(mark => {
+				mark.style.display = "block";
+				mark.classList.add("animated", "fadeIn");
+			});
+		} else {
+			//Если фильтр не передан, показываем информационный блок
+			no.style.display = "block";
+			no.classList.add("animated", "fadeIn");
+		}
+	};
+
+	btnAll.addEventListener("click", () => {
+		typeFilter(markAll);
+	});
+
+	btnLovers.addEventListener("click", () => {
+		typeFilter(markLovers);
+	});
+
+	btnChef.addEventListener("click", () => {
+		typeFilter(markChef);
+	});
+
+	btnGuy.addEventListener('click', () => {
+		typeFilter(markGuy);
+	});
+
+	btnGirl.addEventListener('click', () => {
+		typeFilter(markGirl);
+	});
+
+	btnGrandmother.addEventListener('click', () => {
+		typeFilter();
+	});
+
+	btnGranddad.addEventListener('click', () => {
+		typeFilter();
+	});
+
+	//Переключение активного класса
+	menu.addEventListener('click', (e) => {
+		let target = e.target;
+
+		if (target && target.tagName == "LI") {
+			items.forEach(btn => btn.classList.remove('active'));
+			target.classList.add('active');
+		}
+	});
+};
+
+export default filter;
