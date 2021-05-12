db9e3d47aea94ba4d6a54c0718b62d6566b48a12
Vladislav <kelsenelenelvial@mail.ru>
2021-05-12 15:18:57 +0300

    toggle burger-menu

--- Project №2 (classic style)/src/js/main.js
+++ Project №2 (classic style)/src/js/main.js
@@ -10,6 +10,7 @@ import calc from "./modules/calc";
 import filter from "./modules/filter";
 import pictureSize from "./modules/pictureSize";
 import accordion from "./modules/accordion";
+import burger from "./modules/burger";
 
 window.addEventListener("DOMContentLoaded", () => {
 
@@ -35,4 +36,6 @@ window.addEventListener("DOMContentLoaded", () => {
 	pictureSize(".sizes-block");
 
 	accordion(".accordion-heading");
+
+	burger(".burger-menu", ".burger");
 });
---
+++ Project №2 (classic style)/src/js/modules/burger.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const burger = (menuSelector, burgerSelector) => {
+	const menuElem = document.querySelector(menuSelector),
+		burgerElem = document.querySelector(burgerSelector);
+
+	//Скрываем меню
+	menuElem.style.display = "none";
+
+	burgerElem.addEventListener("click", () => {
+
+		if (menuElem.style.display === "none" && window.screen.availWidth < 993) {
+			/*Если меню скрыто, и ширина экрана(Без панели задач, полосы
+				прокрутки и т.п.) юзера < 993px, то мы его покажем*/
+			menuElem.style.display = "block";
+		} else {
+			//В противном случае скрываем
+			menuElem.style.display = "none";
+
+		}
+	});
+
+	if (window.screen.availWidth > 992) {
+		//Если ширина экрана больше 992px, то скрываем бургер
+		menuElem.style.display = "none";
+	}
+};
+
+export default burger;
