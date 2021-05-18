f7f397b5adbda6d65c257bd8f1f1a8fdd95ab34b
Vladislav <kelsenelenelvial@mail.ru>
2021-05-18 22:15:28 +0300

    mini-sliders on 1,3,5 pages

---
+++ Project №3 (OOP style)/src/js/modules/slider/slider-mini.js
@@ -0,0 +1,99 @@
+"use strict";
+
+import Slider from "./slider";
+
+export default class MiniSlider extends Slider {
+	constructor(container, next, prev, activeClass, animate, autoplay) {
+		super(container, next, prev, activeClass, animate, autoplay);
+	}
+
+	//Стилизация активного слайда
+	decorizeSlides() {
+
+		this.slides.forEach(slide => {
+			//Сначала убираем активный класс со всех слайдов
+			slide.classList.remove(this.activeClass);
+
+			//Если передан параментр анимации в true
+			if (this.animate) {
+				slide.querySelector('.card__title').style.opacity = '0.4';
+				slide.querySelector('.card__controls-arrow').style.opacity = '0';
+			}
+		});
+
+		//Если активный первый слайд это не кнопка
+		if (!this.slides[0].closest('button')) {
+
+			//Добавляем активный класс первому элементу
+			this.slides[0].classList.add(this.activeClass);
+		}
+
+		if (this.animate) {
+
+			this.slides[0].querySelector('.card__title').style.opacity = '1';
+			this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';
+		}
+	}
+
+	nextSlide() {
+
+		/*Условия нужны, т.к. В данном проекте кнопки находятся прямо
+		  внутри слайдера(дочерние элементы). Такой функционал позволяет
+		  избежать добавления активного класса кнопкам при переключении*/
+		if (this.slides[1].tagName == "BUTTON" && this.slides[2].tagName == "BUTTON") {
+
+			this.container.appendChild(this.slides[0]); // Slide
+			this.container.appendChild(this.slides[1]); // Btn
+			this.container.appendChild(this.slides[2]); // Btn
+			this.decorizeSlides();
+		} else if (this.slides[1].tagName == "BUTTON") {
+
+			this.container.appendChild(this.slides[0]); // Slide
+			this.container.appendChild(this.slides[1]); // Btn
+			this.decorizeSlides();
+		} else {
+			//Помещаем первый элемент в конец слайдера
+			this.container.appendChild(this.slides[0]);
+			this.decorizeSlides();
+		}
+	}
+
+	//Привязка кнопок
+	bindTriggers() {
+		this.next.addEventListener('click', () => this.nextSlide());
+
+		this.prev.addEventListener('click', () => {
+
+			for (let i = this.slides.length - 1; i > 0; i--) {
+
+				if (this.slides[i].tagName !== "BUTTON") {
+					//Последний элемент в списке
+					let active = this.slides[i];
+					//Вставляем его перед первым слайдом
+					this.container.insertBefore(active, this.slides[0]);
+					this.decorizeSlides();
+					break;
+				}
+			}
+
+
+		});
+	}
+
+	init() {
+
+		this.container.style.cssText = `
+            display: flex;
+            flex-wrap: wrap;
+            overflow: hidden;
+            align-items: flex-start;
+        `;
+
+		this.bindTriggers();
+		this.decorizeSlides();
+
+		if (this.autoplay) {
+			setInterval(() => this.nextSlide(), 5000);
+		}
+	}
+}
--- Project №3 (OOP style)/src/js/modules/slider/slider.js
+++ Project №3 (OOP style)/src/js/modules/slider/slider.js
@@ -1,16 +1,33 @@
 "use strict";
 
 export default class Slider {
-	constructor({ page = "", btns = "", next = "", prev = "" } = {}) {
+	constructor({ container = null,
+		btns = null,
+		next = null,
+		prev = null,
+		activeClass = "",
+		animate,
+		autoplay } = {}) {
 		//Страница, на которой слайдер
-		this.page = document.querySelector(page);
+		this.container = document.querySelector(container);
 
 		/*Слайды, которые нужно перемещать(в текущем проекте все слайды являются
 		дочерними элементами страницы)*/
-		this.slides = this.page.children;
+		this.slides = this.container.children;
 
 		//Переключатели(стрелки) слайдера
 		this.btns = document.querySelectorAll(btns);
+
+		//Переключатели мини-слайдеров
+		this.next = document.querySelector(next);
+		this.prev = document.querySelector(prev);
+
+		//Активный класс
+		this.activeClass = activeClass;
+		//Анимации
+		this.animate = animate;
+		//Автопереключение слайдов
+		this.autoplay = autoplay;
 
 		//Стартовый слайд
 		this.slideIndex = 1;
