5c3b8fd6358f5d75c0033f3e4284ba765c5154f9
Vladislav <kelsenelenelvial@mail.ru>
2021-06-07 16:35:00 +0300

    complete build v.1

--- UI LIBRARY/src/js/main.js
+++ UI LIBRARY/src/js/main.js
@@ -26,85 +26,85 @@ import $ from "./lib/lib";
 // console.log($(".more").eq(0).siblings());
 // console.log($(".findme").siblings());
 
-$(".findme").fadeIn(5000);
+// $(".findme").fadeIn(5000);
 
-$("#first").on("click", () => {
-	$("div").eq(1).fadeToggle(800);
-});
+// $("#first").on("click", () => {
+// 	$("div").eq(1).fadeToggle(800);
+// });
 
-$("[data-count='second']").on("click", () => {
-	$("div").eq(2).fadeToggle(800);
-});
+// $("[data-count='second']").on("click", () => {
+// 	$("div").eq(2).fadeToggle(800);
+// });
 
-$("button").eq(2).on("click", () => {
-	$(".w-500").fadeToggle(800);
-});
+// $("button").eq(2).on("click", () => {
+// 	$(".w-500").fadeToggle(800);
+// });
 
-$(".wrap").html(
-	`
-	<div class="dropdown">
-			<button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
-			<div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
-				<a href="#" class="dropdown-item">Action #1</a>
-				<a href="#" class="dropdown-item">Action #2</a>
-				<a href="#" class="dropdown-item">Action #3</a>
-			</div>
-	</div>
-	`
-);
-$(".tab-panel .drop").html(
-	`
-	<div class="dropdown">
-			<button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton2">Dropdown button</button>
-			<div class="dropdown-menu" data-toggle-id="dropdownMenuButton2">
-				<a href="#" class="dropdown-item">Action #1</a>
-				<a href="#" class="dropdown-item">Action #2</a>
-				<a href="#" class="dropdown-item">Action #3</a>
-			</div>
-	</div>
-	`
-);
+// $(".wrap").html(
+// 	`
+// 	<div class="dropdown">
+// 			<button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+// 			<div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+// 				<a href="#" class="dropdown-item">Action #1</a>
+// 				<a href="#" class="dropdown-item">Action #2</a>
+// 				<a href="#" class="dropdown-item">Action #3</a>
+// 			</div>
+// 	</div>
+// 	`
+// );
+// $(".tab-panel .drop").html(
+// 	`
+// 	<div class="dropdown">
+// 			<button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton2">Dropdown button</button>
+// 			<div class="dropdown-menu" data-toggle-id="dropdownMenuButton2">
+// 				<a href="#" class="dropdown-item">Action #1</a>
+// 				<a href="#" class="dropdown-item">Action #2</a>
+// 				<a href="#" class="dropdown-item">Action #3</a>
+// 			</div>
+// 	</div>
+// 	`
+// );
 
-$(".dropdown-toggle").dropdown();
+// $(".dropdown-toggle").dropdown();
 
-//Динамическое модальное окно, передав объект настроек
-$("#trigger").click(() => {
-	$("#trigger").createModal({
-		text: {
-			title: "Modal title",
-			body: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat rerum ut
-			recusandae, provident soluta tenetur esse dolorem a.Necessitatibus assumenda officia, vel
-			repudiandae vitae numquam fugiat.Distinctio illo consequuntur veritatis.`
-		},
+// //Динамическое модальное окно, передав объект настроек
+// $("#trigger").click(() => {
+// 	$("#trigger").createModal({
+// 		text: {
+// 			title: "Modal title",
+// 			body: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat rerum ut
+// 			recusandae, provident soluta tenetur esse dolorem a.Necessitatibus assumenda officia, vel
+// 			repudiandae vitae numquam fugiat.Distinctio illo consequuntur veritatis.`
+// 		},
 
-		btns: {
-			count: 3,
-			settings: [
-				["Close",
-					["btn-danger", "mr-10"],
-					true
-				],
-				[
-					"Save changes",
-					["btn-success"],
-					false,
-					() => {
-						alert("Данные сохранены!");
-					}
-				],
-				[
-					"Another button",
-					["btn-warning", "ml-10"],
-					false,
-					() => {
-						alert("Осторожно, что-то пошло не так!");
-					}
-				],
-			],
+// 		btns: {
+// 			count: 3,
+// 			settings: [
+// 				["Close",
+// 					["btn-danger", "mr-10"],
+// 					true
+// 				],
+// 				[
+// 					"Save changes",
+// 					["btn-success"],
+// 					false,
+// 					() => {
+// 						alert("Данные сохранены!");
+// 					}
+// 				],
+// 				[
+// 					"Another button",
+// 					["btn-warning", "ml-10"],
+// 					false,
+// 					() => {
+// 						alert("Осторожно, что-то пошло не так!");
+// 					}
+// 				],
+// 			],
 
-		},
-	});
-});
+// 		},
+// 	});
+// });
 
-$().get('https://jsonplaceholder.typicode.com/todos')
-	.then(result => console.log(result));
+// $().get('https://jsonplaceholder.typicode.com/todos')
+// 	.then(result => console.log(result));
